7efb88c41261b250dba274dfce92cfaaea0d1569
HidDen-MaChinA <hei.franco.3@gmail.com>
2026-01-26 11:23:33 +0100

    feat: add typography component

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,21 @@
+import H1 from "../components/Typography/H1";
+import H2 from "../components/Typography/H2";
+import H3 from "../components/Typography/H3";
+import H4 from "../components/Typography/H4";
+import H5 from "../components/Typography/H5";
+import H6 from "../components/Typography/H6";
+import { Text } from "../components/Typography/Text";
+
+export function LandingPage() {
+   return (
+    <div>
+       <H1>heading 1</H1> 
+        <H2>heading 2</H2>
+        <H3>heading 3</H3>
+        <H4>heading 4</H4>
+        <H5>heading 5</H5>
+        <H6>heading 6</H6>
+        <Text>simple text</Text>
+    </div>
+   ) 
+}
