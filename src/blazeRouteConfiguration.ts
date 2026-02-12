3934de5460dabaabce8f80afdda6ddfb561da571
HidDen-MaChinA <hei.franco.3@gmail.com>
2026-02-12 19:52:59 +0100

    feat: for valentines day xD

--- src/blazeRouteConfiguration.ts
+++ src/blazeRouteConfiguration.ts
@@ -6,32 +6,37 @@ import { LoginPage } from "./pages/LoginPage";
 import createObjectRoutes from "./BlazeRouter/utils/createObjectRoutes";
 import { ForbiddenPage } from "./forbidden_page";
 import { fakeUserAuthMiddleware } from "./middlewares/fakeUserAuthMiddleware";
+import { Valentine } from "./pages/Valentine";
 
 const BlazeObjectRoutes = createObjectRoutes([
-    {
-        path: "/",
-        ComponentPage: LandingPage,
-        Layout: BlazeLayout
-    },
-    {
-        path: "/forms",
-        ComponentPage: FormsPage,
-        Layout: BlazeLayout,
-        protection: true,
-        middlewares: []
-    },
-    {
-        path: "/login",
-        ComponentPage: LoginPage,
-    },
+    // {
+    //     path: "/",
+    //     ComponentPage: LandingPage,
+    //     Layout: BlazeLayout
+    // },
+    // {
+    //     path: "/forms",
+    //     ComponentPage: FormsPage,
+    //     Layout: BlazeLayout,
+    //     protection: true,
+    //     middlewares: []
+    // },
+    // {
+    //     path: "/valentine",
+    //     ComponentPage: Valentine
+    // },
+    // {
+    //     path: "/login",
+    //     ComponentPage: LoginPage,
+    // },
     {
         path: "*",
-        ComponentPage: NotFound
+        ComponentPage: Valentine
     },
-    {
-        path: "/forbidden",
-        ComponentPage: ForbiddenPage
-    }
+    // {
+    //     path: "/forbidden",
+    //     ComponentPage: ForbiddenPage
+    // }
 ])
 
 export default BlazeObjectRoutes
